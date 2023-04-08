ed638706263ae690960acacd6f105290b46affd6
Wesley Mauk <ShadowWesley77@hotmail.com>
2023-04-08 11:02:52 -0400

    Add Quefrency Board

---
+++ keebio-quefrency/keymaps/default/keymap.c
@@ -0,0 +1,29 @@
+#include QMK_KEYBOARD_H
+#define KC_CSE LCTL(LSFT(KC_ESC))
+#define KC_CSP LCTL(LSFT(KC_P))
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_65_with_macro(
+    _______,   KC_MPLY,      QK_GESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,       KC_7,    KC_8,       KC_9,    KC_0,    KC_MINS,   KC_EQL,   KC_DEL,   KC_BSPC, KC_HOME,
+    KC_MPRV,   KC_MNXT,       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                KC_Y,    KC_U,       KC_I,    KC_O,       KC_P,  KC_LBRC,  KC_RBRC,   KC_BSLS, KC_END,
+      KC_F5,     KC_F6,      KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN,  KC_QUOT,   KC_ENT,   KC_PGUP,
+    KC_MUTE,   KC_CALC,      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                KC_N,    KC_M,    KC_COMM,  KC_DOT,    KC_SLSH,  KC_RSFT,    KC_UP,   KC_PGDN,
+     KC_CSE,    KC_CSP,      KC_LCTL,   MO(1), KC_LGUI, KC_LALT,  KC_SPC,                       KC_SPC,  KC_SPC,    KC_RALT, KC_RCTL,    KC_RGUI,  KC_LEFT,  KC_DOWN,   KC_RGHT
+  ),
+
+  [1] = LAYOUT_65_with_macro(
+    _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______, _______,
+    _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,              _______,   _______,    KC_UP,  _______,  _______,  _______,  _______,  _______, _______,
+    _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,              _______,   KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  _______,
+    _______,  _______,      _______,  _______,  _______,  _______,  _______,  _______,              _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______,
+    _______,  _______,      _______,  _______,  _______,  _______,  _______,                        _______,   _______,  _______,  _______,  _______,  _______,  _______,  _______
+  )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+            // Mappings for encoder 1          // Mappings for encoder 2
+    [0] = { ENCODER_CCW_CW(KC_PGUP, KC_PGDN),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+    [1] = { ENCODER_CCW_CW(BL_UP, BL_DOWN),  ENCODER_CCW_CW(KC_VOLD, KC_VOLU)  },
+};
+#endif
